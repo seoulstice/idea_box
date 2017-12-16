8bf12c374f29877bfeab1be1d51bf20e8c4c461a
seoulstice <jungyoungjoon@gmail.com>
2017-12-16 12:39:40 -0700

    Recreated migrations

--- db/migrate/20171216172208_create_ideas.rb
+++ db/migrate/20171216172208_create_ideas.rb
@@ -1,7 +1,8 @@
 class CreateIdeas < ActiveRecord::Migration[5.1]
   def change
     create_table :ideas do |t|
-      t.string :body
+      t.text :body
+      t.references :user, foreign_key: true
     end
   end
 end
