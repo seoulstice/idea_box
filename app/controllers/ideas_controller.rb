efebaf5294aebb36d40a245309a98b990bacedc2
seoulstice <jungyoungjoon@gmail.com>
2017-12-20 19:45:07 -0700

    refactored @categories for the idea new form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,6 +4,7 @@ class IdeasController < ApplicationController
 
   def new
     @idea = Idea.new
+    @categories = Category.order(:classification)
   end
 
   def create
@@ -35,7 +36,7 @@ class IdeasController < ApplicationController
   private
 
     def idea_params
-      params.require(:idea).permit(:body, :category_id, {:image_ids =>[]})
+      params.require(:idea).permit(:body, :category_id, :image_ids =>[])
 
     end
 
