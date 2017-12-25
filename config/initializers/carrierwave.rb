de069f00d16f8bcfba0ab1942544ddbbe9d5e2de
seoulstice <jungyoungjoon@gmail.com>
2017-12-25 16:55:11 -0700

    User show styling

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,13 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 require 'carrierwave/orm/activerecord'
 
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
-      region:                ENV['S3_REGION'],
-  }
-  config.fog_directory  = ENV['S3_BUCKET']
-end
+# CarrierWave.configure do |config|
+#   config.fog_provider = 'fog/aws'
+#   config.fog_credentials = {
+#       provider:              'AWS',
+#       aws_access_key_id:     ENV['S3_KEY'],
+#       aws_secret_access_key: ENV['S3_SECRET'],
+#       region:                ENV['S3_REGION'],
+#   }
+#   config.fog_directory  = ENV['S3_BUCKET']
+# end
