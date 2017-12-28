461c5ecb51a2c7377c537b5d2d8dbad1a581be1d
seoulstice <jungyoungjoon@gmail.com>
2017-12-28 16:04:15 -0700

    fixed problem in search

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -5,6 +5,6 @@ module CollectionsHelper
   end
 
   def term_collection
-    @term_collection ||= ["Short-Term", "Long-Term", "Bucket-List"]
+    @term_collection ||= ["Short-Term", "Long-Term", "Bucket List"]
   end
 end
