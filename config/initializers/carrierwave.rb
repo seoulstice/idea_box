2d2d6737a14b892d7f749ffbb26b1ecdfdcea9ac
seoulstice <jungyoungjoon@gmail.com>
2018-01-01 13:31:02 -0700

    -

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -25,5 +25,4 @@ CarrierWave.configure do |config|
   # config.cache_dir = "#{Rails.root}/tmp/uploads"
 
   config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 end
