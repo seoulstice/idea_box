d22bebe1ecc688cfed62eada8aa108935da7c2f9
seoulstice <jungyoungjoon@gmail.com>
2018-01-01 01:26:57 -0700

    more heroku pt 2

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   mount_uploader :image, ImageUploader
-  has_many :concert_images, dependent: :destroy
-  has_many :concerts, through: :concert_images
+  has_many :concert_images
+  has_many :concerts, through: :concert_images, dependent: :destroy
 
   def self.per_page
     10
